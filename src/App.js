17ec903c0fe6251955ad2d5dcc499ad8e459fca8
Darron Haworth <darron.haworth@firsttechfed.com>
2016-11-19 11:16:55 -0800

    add in firebase and eslint + eslint config rules

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import React, {Component} from 'react';
-import {View, Text} from 'react-native';
-import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import firebase from 'firebase';
 import reducers from './reducers';
 
 class App extends Component {
+    componentWillMount() {
+        const config = {
+            apiKey: 'AIzaSyDYvfp4yCNxiyF-39mPrAC0fqdRkOTaLvU',
+            authDomain: 'udemy-rn-auth.firebaseapp.com',
+            databaseURL: 'https://udemy-rn-auth.firebaseio.com',
+            storageBucket: 'udemy-rn-auth.appspot.com',
+            messagingSenderId: '454126433225'
+        };
+        firebase.initializeApp(config);
+    }
     render() {
         return (
             <Provider store={createStore(reducers)}>
-                <View>
-                    <Text> 
-                        Hellow
+                <View style={{ marginTop: 20 }}>
+                    <Text>
+                        Hello
                     </Text>
                 </View>
             </Provider>
