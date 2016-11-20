fad71ed9b60e1e1eddcd6455f302ca97c04f58d3
Darron Haworth <darron.haworth@firsttechfed.com>
2016-11-20 13:28:53 -0800

    wired up email action, reducer: /learn/v4/t/lecture/5870400

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,36 +1,50 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Card, CardSection, Input, Button } from './common';
 import { emailChanged } from '../actions';
+import { Card, CardSection, Input, Button } from './common';
 
 class LoginForm extends Component {
+  onEmailChange(text) {
+    this.props.emailChanged(text);
+  }
 
-    onEmailChange(text) {
-        emailChanged(text);
-    }
-    render() {
-        return (
-            <Card>
-                <CardSection>
-                    <Input 
-                    label="Email" 
-                    placeholder="email@gmail.com"
-                    onChangeText={this.onEmailChange.bind(this)}
-                    value={this.props.email}
-                    />
-                </CardSection>
 
-                <CardSection>
-                <Input label="Password" placeholder="password" />
-                </CardSection>
+  render() {
+    return (
+      <Card>
+        <CardSection>
+          <Input
+            label="Email"
+            placeholder="email@gmail.com"
+            onChangeText={this.onEmailChange.bind(this)}
+            value={this.props.email}
+          />
+        </CardSection>
 
-                <CardSection>
-                    <Button>Login</Button>
-                </CardSection>
-            </Card>
-        );
-    }
+        <CardSection>
+          <Input
+            secureTextEntry
+            label="Password"
+            placeholder="password"
+            value={this.props.password}
+          />
+        </CardSection>
+
+        <CardSection>
+            <Button>Log In</Button>
+        </CardSection>
+      </Card>
+    );
+  }
 }
+
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red'
+  }
+};
 
 const mapStateToProps = ({ auth }) => {
   const { email } = auth;
