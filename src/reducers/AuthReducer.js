bfef8fe1b8416f335ff363934a6afa451f59298c
Darron Haworth <darron.haworth@firsttechfed.com>
2016-11-20 21:10:53 -0800

    more actions, reducers, state, and ui - login works: /learn/v4/t/lecture/5870462

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,17 +1,31 @@
-import { EMAIL_CHANGED, PASSWORD_CHANGED } from '../actions/types';
+import { 
+    EMAIL_CHANGED, 
+    PASSWORD_CHANGED,
+    LOGIN_USER_START,
+    LOGIN_USER_SUCCESS,
+    LOGIN_USER_FAIL
+ } from '../actions/types';
 
 const INITIAL_STATE = { 
     email: '', 
-    password: '' 
+    password: '',
+    user: null,
+    loading: null,
+    error: ''
 };
 
 export default (state = INITIAL_STATE, action) => {
-    switch (action.type) {
-        
+    switch (action.type) {        
         case EMAIL_CHANGED:        
             return { ...state, email: action.payload };
         case PASSWORD_CHANGED:
             return { ...state, password: action.payload };
+        case LOGIN_USER_START:
+            return { ...state, loading: true, error: '' };
+        case LOGIN_USER_SUCCESS:
+            return { ...state, loading: false, user: action.payload, error: '' };
+        case LOGIN_USER_FAIL:
+            return { ...state, loading: false, error: 'Authentication Failed', password: '' };
         default:
         return state;
     }
