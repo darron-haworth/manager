464b88950277a9c492344c0c63335534669e28df
Darron Haworth <darron.haworth@firsttechfed.com>
2016-11-23 18:57:14 -0800

    initial nav: /learn/v4/t/lecture/5870500

--- src/Router.js
+++ src/Router.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Scene, Router } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
+import EmployeeList from './components/EmployeeList';
 
 const RouterComponent = () => {
     return (
-        <Router>
-            <Scene
-                sceneStyle={{ paddingTop: 65 }}
-                key="login" 
-                component={LoginForm} 
-                title="Please Login"
-            />
+        <Router sceneStyle={{ paddingTop: 65 }}>
+            <Scene key="auth">
+                <Scene component={LoginForm} initial key="login" title="Please Login" />
+            </Scene>
+            <Scene key="main">
+                <Scene component={EmployeeList} initial key="employeeList" title="Employees" />
+            </Scene>
         </Router>
     );
 };
