97055382e73af5ba3c02a2bde0b791f8bf698218
Darron Haworth <darron.haworth@firsttechfed.com>
2016-11-23 20:17:41 -0800

    rough out a form

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text } from 'react-native';
+import { Card, CardSection, Input, Button } from './common';
 
 class EmployeeCreate extends Component {
     render() {
         return (
-            <View>
-                <Text>Employee Form</Text>
-            </View>
+            <Card>
+                <CardSection>
+                    <Input
+                    label="Name"
+                    placeholder="Jane"
+                    />
+                </CardSection>
+
+                <CardSection>
+                 <Input
+                    label="Phone"
+                    placeholder="555-555-1212"
+                 />
+                 </CardSection>
+
+                 <CardSection>
+                    <Text>Picker</Text>
+                 </CardSection>
+
+
+                <CardSection>
+                    <Button>
+                        Create
+                    </Button>
+                </CardSection>
+            </Card>
         );
     }
 }
