7893fd0100fa44d1b5639a7016590a7c0d56319a
Darron Haworth <darron.haworth@firsttechfed.com>
2016-11-23 20:33:33 -0800

    started employeeform reducer, actions, etc: /learn/v4/t/lecture/5870526

---
+++ src/reducers/EmployeeFormReducer.js
@@ -0,0 +1,21 @@
+import {
+    EMPLOYEE_UPDATE
+} from '../actions/types';
+
+const INITIAL_STATE = {
+    name: '',
+    phone: '',
+    shift: ''
+ };
+
+export default (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+        case EMPLOYEE_UPDATE:
+            // action.payload === { prop: 'name', value: 'jane' }
+            // the [] below is not an array, it is 'key interpolation', 
+            // the prop is evaluated as the prop sent it 'name' in this example
+            return { ...state, [action.payload.prop]: action.payload.value };
+        default:
+        return state;
+    }
+};
