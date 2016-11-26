fc738ac50ff22b9fee65240a4bcba0b382a0847d
Darron Haworth <darron.haworth@firsttechfed.com>
2016-11-26 14:50:31 -0800

    added touchable to list to link to create page

--- src/components/EmployeeListItem.js
+++ src/components/EmployeeListItem.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Actions } from 'react-native-router-flux';
+import { Text, TouchableWithoutFeedback, View } from 'react-native';
 import { CardSection } from './common';
 
 class EmployeeListItem extends Component {
+
+    onRowPress() {
+        Actions.employeeCreate();
+    }
+
     render() {
         const { name } = this.props.employee;
 
         return (
-            <CardSection>
-                <Text style={{ fontSize: 18, paddingLeft: 15 }}>
-                    {name}
-                </Text>
-            </CardSection>
+            <TouchableWithoutFeedback onPress={this.onRowPress.bind(this)} >
+                <View>
+                    <CardSection>
+                        <Text style={{ fontSize: 18, paddingLeft: 15 }}>
+                            {name}
+                        </Text>
+                    </CardSection>
+                </View>
+            </TouchableWithoutFeedback>
         );
     }
 }
