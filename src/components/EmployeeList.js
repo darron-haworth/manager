c01913395fc17220db3556382dc14fbda222522c
Darron Haworth <darron.haworth@firsttechfed.com>
2016-11-25 20:19:14 -0800

    wire up  componentWillMount, .willReceiveProps, and mapStateToProps

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,8 +1,33 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import _ from 'lodash';
+import { connect } from 'react-redux';
+import { ListView, Text, View } from 'react-native';
+import { employeesFetch } from '../actions'; 
 
 class EmployeeList extends Component {
+
+    componentWillMount() {
+        this.props.employeesFetch();
+        this.createDataSource(this.props);
+    }
+
+    componentWillReceiveProps(nextProps) {
+        //nextProps are the next set of props that this componentWillMount
+        //will be rendered with
+        //this.props is still the old set of props
+        this.createDataSource(nextProps);
+    }
+
+    createDataSource({ employees }) {
+        const ds = new ListView.DataSource({
+            rowHasChanged: (r1, r2) => r1 !== r2
+        });
+
+        this.dataSource = ds.cloneWithRows(employees);
+    }
+
     render() {
+        console.log(this.props);
         return (
             <View>
                 <Text>Employee 1</Text>
@@ -16,5 +41,11 @@ class EmployeeList extends Component {
     }
 }
 
-export default EmployeeList;
+const mapStateToProps = state => {
+    const employees = _.map(state.employees, (val, uid) => {
+        return { ...val, uid };
+    });
+    return { employees };
+};
 
+export default connect(mapStateToProps, { employeesFetch })(EmployeeList);
