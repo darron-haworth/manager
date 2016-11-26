37ac19683a732d94ab0474d9b9c0c4bae7315501
Darron Haworth <darron.haworth@firsttechfed.com>
2016-11-25 19:23:32 -0800

    short circuit thunk and nav back to list: /learn/v4/t/lecture/5870552

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,4 +1,5 @@
 import firebase from 'firebase';
+import { Actions } from 'react-native-router-flux';
 
 import {
     EMPLOYEE_UPDATE
@@ -12,7 +13,12 @@ export const employeeUpdate = ({ prop, value }) => {
     };
 };
 
+
 export const employeeCreate = ({ name, phone, shift }) => {
     const { currentUser } = firebase.auth();
-    firebase.database().ref(`/users/${currentUser.uid}/employees`).push({ name, phone, shift });
+    return () => {
+        firebase.database().ref(`/users/${currentUser.uid}/employees`)
+        .push({ name, phone, shift })
+        .then(() => Actions.employeeList({ type: 'reset' }));
+    };
 };
