1175caac065367fc79ca8a37cceba802bb1b4af0
Darron Haworth <darron.haworth@firsttechfed.com>
2016-11-27 08:34:45 -0800

    start building out confirm box

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -3,11 +3,13 @@ import React, { Component } from 'react';
 import { text } from 'react-native-communications';
 import { connect } from 'react-redux';
 import { employeeUpdate, employeeSave } from '../actions';
-import { Card, CardSection, Button } from './common';
+import { Card, CardSection, Button, Confirm } from './common';
 import EmployeeForm from './EmployeeForm';
 
 
 class EmployeeEdit extends Component {
+
+    state = { showModal: false };
 
     componentWillMount() {
         _.each(this.props.employee, (value, prop) => {
@@ -39,6 +41,16 @@ class EmployeeEdit extends Component {
                     Text Schedule
                     </Button>
                 </CardSection>
+
+                <CardSection>
+                    <Button onPress={() => this.setState({ showModal: !this.state.showModal })}>
+                        Fire Employee
+                    </Button>
+                </CardSection>
+
+                <Confirm visible={this.state.showModal}>
+                    Are you sure you want to delete this?
+                </Confirm>
             </Card>
         );
     }
