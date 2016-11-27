1f13fd33a3a61ae28b8c8fad4e780dc9f99f6dd3
Darron Haworth <darron.haworth@firsttechfed.com>
2016-11-27 11:22:09 -0800

    remove console.warn

--- src/reducers/EmployeeFormReducer.js
+++ src/reducers/EmployeeFormReducer.js
@@ -14,7 +14,6 @@ const INITIAL_STATE = {
 export default (state = INITIAL_STATE, action) => {
     switch (action.type) {
         case EMPLOYEE_EDIT_CLEAR:
-            console.warn('clear');
             return INITIAL_STATE;
         case EMPLOYEE_CREATE:
             return INITIAL_STATE;
